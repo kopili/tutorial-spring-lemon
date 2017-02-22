aa92059e8031b280e8640f319df40bda1439881b
Borislav Kirilov <bobikirilov@gmail.com>
2017-02-22 14:24:18 +0200

    finished customizing the application and it should be ready now

--- src/main/java/com/naturalprogramming/spring/lemon/mvc/tutorial/entity/User.java
+++ src/main/java/com/naturalprogramming/spring/lemon/mvc/tutorial/entity/User.java
@@ -1,7 +1,11 @@
 package com.naturalprogramming.spring.lemon.mvc.tutorial.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 import com.naturalprogrammer.spring.lemon.domain.AbstractUser;
 
@@ -9,5 +13,20 @@ import com.naturalprogrammer.spring.lemon.domain.AbstractUser;
 @Table(name = "usr")
 public class User extends AbstractUser<User, Long> {
 	private static final long serialVersionUID = 2716710947175132319L;
+
+	public static final int NAME_MIN = 1;
+	public static final int NAME_MAX = 50;
+	@NotBlank(message = "{blank.name}", groups = { SignUpValidation.class, UpdateValidation.class })
+	@Size(min = NAME_MIN, max = NAME_MAX, groups = { SignUpValidation.class, UpdateValidation.class })
+	@Column(nullable = false, length = NAME_MAX)
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
--- src/main/java/com/naturalprogramming/spring/lemon/mvc/tutorial/services/MyService.java
+++ src/main/java/com/naturalprogramming/spring/lemon/mvc/tutorial/services/MyService.java
@@ -3,6 +3,7 @@ package com.naturalprogramming.spring.lemon.mvc.tutorial.services;
 import org.springframework.stereotype.Service;
 
 import com.naturalprogrammer.spring.lemon.LemonService;
+import com.naturalprogrammer.spring.lemon.util.LemonUtil;
 import com.naturalprogramming.spring.lemon.mvc.tutorial.entity.User;
 
 @Service
@@ -11,4 +12,30 @@ public class MyService extends LemonService<User, Long> {
 	protected User newUser() {
 		return new User();
 	}
+
+	@Override
+	protected void updateUserFields(User user, User updatedUser, User currentUser) {
+		super.updateUserFields(user, updatedUser, currentUser);
+		user.setName(updatedUser.getName());
+		LemonUtil.afterCommit(() -> {
+			if (currentUser.equals(user))
+				currentUser.setName(user.getName());
+		});
+	}
+
+	@Override
+	protected User createAdminUser() {
+		User user = super.createAdminUser();
+		user.setName("Administrator");
+		return user;
+	}
+
+	@Override
+	protected User userForClient(User currentUser) {
+		User user = super.userForClient(currentUser);
+		if (user != null)
+			user.setName(currentUser.getName());
+		return user;
+	}
+
 }
